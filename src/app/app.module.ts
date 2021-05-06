51432f183d98529b0f056ca6ec2b2c074c9ef421
hamzah khan <khanha3@aston.ac.uk>
2021-05-06 13:29:13 +0100

    quick clean up of ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShoppingListComponentComponent } from './components/shopping-list-component/shopping-list-component.component';
-import { ListItem } from  './models/list-item';
+import { ListItem } from './models/list-item';
 import { FormsModule } from '@angular/forms';
-
 
 @NgModule({
   declarations: [
--- src/app/components/shopping-list-component/shopping-list-component.component.ts
+++ src/app/components/shopping-list-component/shopping-list-component.component.ts
@@ -18,63 +18,69 @@ export class ShoppingListComponentComponent implements OnInit {
   currentItemInput: string;
   previousItemInput: string;
 
-
   constructor() { }
 
   ngOnInit() {
   }
 
   addToCurrentList(): void {
-    let duplicate: boolean = false;
-    for(var i = 0;i < this.currentlist.length; i++){
-      if(this.currentItemInput == this.currentlist[i].ItemName){
-          alert("already added to list");
-          duplicate = true;
-          break;
-      }
-    }
-    if(!duplicate){
-    this.currentlist.push({ItemID:(new Date()).getTime(), ItemName: this.currentItemInput , ListID: 0 , HighPriority: false , Index:0});
-    }
-  }
-
-  addToPreviousList(): void {
-    let duplicate: boolean = false;
-
-    for (var i = 0; i < this.previouslist.length; i++) {
-      if (this.previousItemInput === this.previouslist[i].ItemName) {
-        alert("already added to list");
+    let duplicate = false;
+    for (var i = 0; i < this.currentlist.length; i++) {
+      if (this.currentItemInput == this.currentlist[i].ItemName) {
+        alert('already added to list');
         duplicate = true;
         break;
       }
     }
-       if (!duplicate) {
-          this.previouslist.push({ ItemID: (new Date()).getTime(), ItemName: this.previousItemInput, ListID: 1, HighPriority: false, Index: 0 });
-        }
+    if (!duplicate) {
+      this.currentlist.push({ ItemID: (new Date()).getTime(), ItemName: this.currentItemInput, ListID: 0, HighPriority: false, Index: 0 });
+    }
+  }
 
-}
+  addToPreviousList(): void {
+    let duplicate = false;
+
+    for (var i = 0; i < this.previouslist.length; i++) {
+      if (this.previousItemInput === this.previouslist[i].ItemName) {
+        alert('already added to list');
+        duplicate = true;
+        break;
+      }
+    }
+    if (!duplicate) {
+      this.previouslist.push({
+        ItemID: (new Date()).getTime(),
+        ItemName: this.previousItemInput,
+        ListID: 1,
+        HighPriority: false,
+        Index: 0
+      });
+    }
+  }
 
   onDeleteCurrentList(item): void {
     console.log(item)
-    for(var i = 0;i < this.currentlist.length; i++){
-      if(item == this.currentlist[i].ItemName){
-          this.currentlist.splice(i,1);
-          break;
+    for (var i = 0; i < this.currentlist.length; i++) {
+      if (item === this.currentlist[i].ItemName) {
+        this.currentlist.splice(i, 1);
+        break;
       }
     }
   }
+
   onDeletePreviousList(item): void {
     console.log(item);
-    for(var i = 0;i < this.previouslist.length; i++){
-      if(item == this.previouslist[i].ItemName){
-          this.previouslist.splice(i,1);
-          break;
+    for (var i = 0; i < this.previouslist.length; i++) {
+      if (item === this.previouslist[i].ItemName) {
+        this.previouslist.splice(i, 1);
+        break;
       }
     }
   }
+
   onEditCurrentList(item): void {
     for (var i = 0; i < this.currentlist.length; i++) {
-      if (item == this.currentlist[i].ItemName) {
+      if (item === this.currentlist[i].ItemName) {
         this.currentlist[i].ItemName = this.currentItemInput;
         break;
       }
